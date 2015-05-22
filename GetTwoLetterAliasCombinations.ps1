cbc626ce61e8d6e4228a25d1f4f5508573c8be1e
coldenough <vrednopolezno@gmail.com>
2015-05-22 15:53:06 -0700

    Added two foreach loops

--- GetTwoLetterAliasCombinations.ps1
+++ GetTwoLetterAliasCombinations.ps1
@@ -2,3 +2,10 @@
 $asciiNum = 97..122
 $letters = $asciiNum | ForEach-Object { [char]$_ }
 $letters
+
+Foreach ($1letter in $letters) {
+  Foreach ($2letter in $letters) {
+    [array]$letterCombinations += "$1letter$2letter"
+  }
+}
+$letterCombinations
